308ff879882814f75fe3ea0dc1646c5d32b726ad
canisminor1990 <canisminor1990@gmail.com>
2016-12-13 11:10:26 +0800

    autobuild

--- dist/dynmap/gettiles.php
+++ dist/dynmap/gettiles.php
@@ -6,7 +6,7 @@ $path = $_SERVER['PATH_INFO'];
 $fname = TILESPATH . $path;
 
 if (!file_exists($fname)) {
-  $fname = "images/blank.png";
+  $fname = "//canisminor.cc/img/dynmap/blank.png";
 }
 $fp = fopen($fname, 'rb');
 if (strstr($path, ".png"))

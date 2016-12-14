44a7247197408b10d831a45294af3cb5fed381de
canisminor1990 <canisminor1990@gmail.com>
2016-12-14 11:39:28 +0800

    autobuild

--- _js/server.js
+++ _js/server.js
@@ -1,37 +1,46 @@
+function getMinecraftTime(servertime) {
+    servertime = parseInt(servertime);
+    var day = servertime >= 0 && servertime < 13700;
+    return {
+        servertime: servertime,
+        days: parseInt((servertime + 8000) / 24000),
+
+        // Assuming it is day at 6:00
+        hours: (parseInt(servertime / 1000) + 6) % 24,
+        minutes: parseInt(((servertime / 1000) % 1) * 60),
+        seconds: parseInt(((((servertime / 1000) % 1) * 60) % 1) * 60),
+
+        day: day,
+        night: !day
+    };
+}
+
 $(function () {
     var className = $("server");
     var strHtml = "";
-    $.getJSON("https://mcapi.ca/query/canisminor.cc/info", function (json) {
-        //html
-        strHtml +=
-            "<div class='w-card'>" +
-            "<div class='w-avatar'><img src='";
-
-        strHtml += json.favicon;
-
-        strHtml += "'></div>" +
-            "<div class='w-box'>" +
-            "<div class='w-title'>" + Server + ":<a href='//canisminor.cc'>" +
-            json.hostname + "</a></div>" +
-            "<div class='w-time'><span>" +
-            json.ref.split("/")[2] +
-            "</span>" + time + "</div>";
-
-        for (i in json.commits) {
-            strHtml +=
-                "<a class='w-message' href='" + json.commits[i].web_url + "'>" +
-                json.commits[i].short_message + "</a>" +
-                "<div class='w-author'>committer: " + "<span>" +
-                json.commits[i].committer.name + "</span></div>";
-        }
-
-        strHtml += "</div>";
-        for (var i = 0; i < projects.length; i++) {
-            if (url == projects[i]) {
-                strHtml += "<div class='auto-build'>客户端更新</div>";
-            }
-        }
-        strHtml += "</div>";
-        className.html(strHtml)
+    $.getJSON("http://map.canisminor.cc/up/world/world/", function (json) {
+        var json = json;
+        return json;
     })
+    var servertime = getMinecraftTime(json.servertime),
+        nowtime = servertime.hours + ":" + servertime.minutes + ":" + servertime.seconds
+    //html
+    strHtml +=
+        "<div class='w-card'>" +
+        "<div class='w-avatar'><img src='//canisminor.cc/img/logo.png'></div>" +
+        "<div class='w-box'>" +
+        "<div class='w-title'>Server:<a href='//canisminor.cc'>Canisminor Craft</a></div>" +
+        "<div class='w-time'><span>" +
+        json.currentcount + "/20" +
+        "</span>" + nowtime + "</div>";
+    strHtml += "</div>";
+
+    if (json) {
+        strHtml += "<div class='auto-build'>Online</div>";
+    } else {
+        strHtml += "<div class='auto-build offline'>Offline</div>";
+    }
+
+    strHtml += "</div>";
+    className.html(strHtml)
 })

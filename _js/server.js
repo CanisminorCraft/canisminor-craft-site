f2cb3716dcc7166f8e4bfcef58e0e92b9662266b
canisminor1990 <canisminor1990@gmail.com>
2016-12-14 12:10:55 +0800

    autobuild

--- _js/server.js
+++ _js/server.js
@@ -15,16 +15,11 @@ function getMinecraftTime(servertime) {
     };
 }
 
-function motd() {
-    var motd = $.getJSON("http://mcapi.ca/query/canisminor.cc/motd", function (json) {
-        return json.motd
-    })
-    return motd
-}
 
 $(function () {
     var className = $("#server");
     var strHtml = "";
+
     $.getJSON("http://map.canisminor.cc/up/world/world/", function (json) {
         var servertime = getMinecraftTime(json.servertime),
             nowtime = servertime.hours + ":" + servertime.minutes + ":" + servertime.seconds
@@ -35,16 +30,31 @@ $(function () {
             "<div class='w-box'>" +
             "<div class='w-title'>Server:<a href='//canisminor.cc'>Canisminor Craft</a></div>" +
             "<div class='w-time'><span>" + json.currentcount + "/20" + "</span>" + nowtime + "</div></div>";
-        strHtml += "<div class='w-message'>" + motd() + "</div>";
-
+        strHtml += "<div class='w-message'></div>";
+        //player
+        strHtml += "<div class='p-card>";
+        if (json.currentcount > 0) {
+            for (var i = 0; i < json.players.length; i++) {
+                strHtml += "<div class='p-box>";
+                strHtml += "<div class='w-avata'><img src='http://map.canisminor.cc/tiles/faces/32x32/" + json.players[i].name + "'></div>";
+                strHtml += "</div>"
+            }
+        } else {
+            strHtml += "<div class='no-player'>暂无玩家在线</div>"
+        }
+        strHtml += "</div>"
+        // state
         if (json) {
             strHtml += "<div class='auto-build'>Online</div>";
         } else {
             strHtml += "<div class='auto-build offline'>Offline</div>";
         }
-
         strHtml += "</div>";
         className.html(strHtml)
+    })
+
+    $.getJSON("http://mcapi.ca/query/canisminor.cc/motd", function (json) {
+        $("#server .w-message").text(json.motd)
     })
 
 })
